f8defc50600014f1eeb9d113c51a916a581317fb
Yudi Sun <sunyudi1@ecf.utoronto.ca>
2021-06-22 21:21:33 -0400

    n

---
+++ AlgoTradingTrill/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Runtime.InteropServices;
+using System.Xml.Xsl;
+using Microsoft.StreamProcessing;
+
+namespace AlgoTradingTrill
+{
+    class Program
+    {
+        class StockPriceDaily
+        {
+            public long Date;
+            public double Open;
+            public double High;
+            public double Low;
+            public double Close;
+            public long Volume;
+            public string Name;
+
+            public StockPriceDaily(long date, double open, double high, double low, double close, long volume, string name)
+            {
+                this.Date = date;
+                this.Open = open;
+                this.High = high;
+                this.Low = low;
+                this.Close = close;
+                this.Volume = volume;
+                this.Name = name;
+            }
+        }
+        
+        class MyObservable : IObservable<StockPriceDaily>
+        {
+            public IDisposable Subscribe(IObserver<StockPriceDaily> observer)
+            {
+                //using (var reader = new StreamReader(@"C:\Users\yudis\Documents\university\Summer2021\Code\pysparkPrograms\all_stock_UNIX_sorted.csv"))
+                using (var reader = new StreamReader(@"/root/AlgorithmicTradingSimulation/1_thousand_stock_UNIX.csv"))
+                {
+                    reader.ReadLine();
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        var values = line.Split(',');
+                        //Console.WriteLine(line);
+                        var data = new StockPriceDaily(long.Parse(values[0]),Convert.ToDouble(values[1]),Convert.ToDouble(values[2]),
+                            Convert.ToDouble(values[3]), Convert.ToDouble(values[4]),long.Parse(values[5]), values[6]);
+                        observer.OnNext(data);
+
+                    }
+                }
+                observer.OnCompleted();
+                return Disposable.Empty;
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            //var stockRecordObservable = new[]
+            //{
+            //    //This should be done in a separate file, but I still need to learn how to read files in C#.
+            //    new StockPriceDaily(1483401600, 62.79, 62.84, 62.12, 62.58, 20694101, "MSFT"),
+            //    new StockPriceDaily(1483488000, 62.48,62.75,62.12,62.3,21339969,"MSFT"),
+            //    new StockPriceDaily(1483574400, 62.19,62.66,62.03,62.3,21339969,"MSFT"),
+            //    new StockPriceDaily(1483660800, 62.3,63.15,62.04,62.84,21339969,"MSFT"),
+            //    new StockPriceDaily(1483920000, 62.76,63.08,62.54,62.64,21339969,"MSFT"),
+            //    new StockPriceDaily(1484006400, 62.73,63.07,62.28,62.62,21339969,"MSFT"),
+            //    new StockPriceDaily(1484092800, 62.61,63.23,62.43,63.19,21339969,"MSFT"),
+            //    new StockPriceDaily(1484179200, 63.06,63.4,61.95,62.61,21339969,"MSFT"),
+            //    new StockPriceDaily(1484265600, 62.62,62.86,62.35,62.7,21339969,"MSFT"),
+            //    new StockPriceDaily(1484611200, 62.68,62.7,62.03,62.53,21339969,"MSFT"),
+            //    new StockPriceDaily(1484697600, 62.67,62.7,62.12,62.5,21339969,"MSFT"),
+            //    new StockPriceDaily(1484784000, 62.24,62.98,62.2,62.3,21339969,"MSFT"),
+            //    new StockPriceDaily(1484870400, 62.67,62.82,62.37,62.74,21339969,"MSFT"),
+            //    new StockPriceDaily(1485129600, 62.7,63.12,62.57,62.96,21339969,"MSFT"),
+            //    new StockPriceDaily(1485216000, 63.2,63.74,62.94,63.52,21339969,"MSFT"),
+            //    new StockPriceDaily(1485302400, 63.95,64.1,63.45,63.68,21339969,"MSFT"),
+            //    new StockPriceDaily(1485388800, 64.12,64.54,63.55,64.27,21339969,"MSFT"),
+            //    new StockPriceDaily(1485475200, 65.39,65.91,64.89,65.78,21339969,"MSFT"),
+            //    new StockPriceDaily(1485734400, 65.69,65.79,64.8,65.13,21339969,"MSFT"),
+            //    new StockPriceDaily(1485820800, 64.86,65.15,64.26,64.65,21339969,"MSFT")
+            //}.ToObservable();
+            
+            //var stockRecordStreamable =
+            //    stockRecordObservable.Select(e => StreamEvent.CreateInterval(e.Date, e.Date + 86400, e))
+            //        .ToStreamable(DisorderPolicy.Drop());
+            
+            
+            var stockRecordObservable = new MyObservable();
+            var stockRecordStreamable =
+                stockRecordObservable.ToTemporalStreamable(e => e.Date, e => e.Date + 86400)
+                    .Cache();
+
+            var sw = new Stopwatch();
+            sw.Start();
+            
+            var allPrices =
+                stockRecordStreamable.Select(e => new{e.Open, e.High, e.Low, e.Close, e.Name});
+
+            var movingAverages = stockRecordStreamable.GroupApply(e => e.Name,
+                s => s.Multicast(
+                    t => t.HoppingWindowLifetime(4320000, 86400)
+                        .Average(e => e.Close).Join(t
+                                .HoppingWindowLifetime(1728000, 86400).Average(e => e.Close),
+                            (ma50, ma20) => new {ma50, ma20})),
+                (g, p) => new {name = g.Key, averages = p}
+            );
+
+            var result = movingAverages.Multicast(s => s.Join(s.Select(e => e.averages.ma20 > e.averages.ma50),
+                (left, buy) => new {left, buy}));
+               
+            
+            //result
+            //    .ToStreamEventObservable() 
+            //    .Where(e => e.IsData) 
+            //    .ForEachAsync(e => Console.WriteLine(e.ToString()));
+            
+            
+            result
+                .ToStreamEventObservable()
+                .Wait();
+            
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed.TotalSeconds);
+        }
+    }
+    
+
+}
--- algo_trading_revised.py
+++ algo_trading_revised.py
@@ -4,16 +4,19 @@ from pyspark.sql import SparkSession
 import pyspark.sql.functions as F
 from pyspark.sql.types import *
 import time
+from numpy import searchsorted
 
 if __name__ == "__main__":
     spark = SparkSession\
-    .builder.appName("AlgoTrading").getOrCreate()
+    .builder.appName("AlgoTrading").config('spark.sql.codegen.wholeStage', 'false').getOrCreate()
     
     spark.sparkContext.setLogLevel("ERROR")
     #spark.sparkContext.setLogLevel("TRACE")
 
+    spark.conf.set("spark.sql.session.timeZone", "UTC")
+
     
-    schema = StructType([StructField("Date", DateType(), True),\
+    schema = StructType([StructField("Date", LongType(), True),\
         StructField("Open", DoubleType(), True),\
         StructField("High", DoubleType(), True),\
         StructField("Low", DoubleType(), True),\
@@ -30,37 +33,57 @@ if __name__ == "__main__":
         .schema(schema)\
         .load()
 
-    withTime = Df1.withColumn("DateTime", F.unix_timestamp('Date', 'yyyy-MM-dd').cast('timestamp')) # add a timestamp column because streaming window aggregation requires timestamp type
+    #withTime = Df1.withColumn("DateTime", F.unix_timestamp('Date', 'yyyy-MM-dd').cast('timestamp')) # add a timestamp column because streaming window aggregation requires timestamp type
+    withTime = Df1.withColumn('DateTime', F.from_unixtime('Date').cast('timestamp'))
 
-
-    def ma10(past20days):
+    def ma20(past50days):
         #past20days: [struct['DateTime': Timestamp, 'Close': Double, 'window.end': Timestamp]]
-        cutoff = past20days[0][2] - timedelta(days=10)
+        cutoff = past50days[0][2] - timedelta(days=21)
+
         filtered = []
-        for i in range(len(past20days)):
-            if past20days[i][0] > cutoff:
-                filtered.append(past20days[i][1])
+        for i in range(len(past50days)):
+            if past50days[i][0] > cutoff:
+                filtered.append(past50days[i][1])
         if not filtered:
             return None
+
+        #times = [row[0] for row in past50days]
+        #i = searchsorted(times, cutoff, side='left')
+        #if i == len(past50days):
+        #    return None
+        #filtered = [row[1] for row in past50days[i:]]
+
         return mean(filtered)
     
-    ma10udf = F.udf(ma10, DoubleType())
+    ma20udf = F.udf(ma20, DoubleType())
 
     
-    movingAverage = withTime\
-        .groupBy(F.col('Name'), F.window(F.col('DateTime'), "20 days", "1 day"))\
-                .agg(ma10udf(F.collect_list(F.struct("DateTime", "Close", "window.end"))).alias("ma_10_days"), \
-                    F.mean("Close").alias("ma_20_days"))\
-                    .select('Name', F.to_date('window.end').alias('ResultDate'), 'ma_10_days', 'ma_20_days')\
+    movingAverage = withTime.withWatermark('DateTime', "1 second")\
+        .groupBy(F.col('Name'), F.window(F.col('DateTime'), "50 days", "1 day"))\
+                .agg(ma20udf(F.collect_list(F.struct("DateTime", "Close", "window.end"))).alias("ma_20_days"), \
+                    F.mean("Close").alias("ma_50_days"))\
+                    .select('Name', F.to_date('window.end').alias('ResultDate'), 'ma_20_days', 'ma_50_days')\
                         .orderBy('ResultDate')
+
+    #   
+    # if no watermark is specified, all intermediate state will be stored in memory and all out of order (delayed) events will be handled until memory runs out.
+    
+    # if a watermark is specified, events that are delayed past the watermark will be dropped. Whether an event is delayed past the watermark
+    # depends on the window it belongs to. If the (maximum eventTime seen by the stream engine) - (watermark) > (endTime of the window the event belongs to),
+    # then the event is dropped for that window. Since the event can belong to multiple windows in the case of sliding windows (as opposed to 
+    # tumbling windows), it may still be accounted for in a later window.
+    
+    # ORDERBY CAN ONLY BE USED IN COMPLETE OUTPUT MODE
+
 
 
     # generates BUY signal if 10-day moving average is greater than 20-day moving average
-    result = movingAverage.withColumn("Buy", F.col('ma_10_days') > F.col('ma_20_days'))
+    result = movingAverage.withColumn("Buy", F.col('ma_20_days') > F.col('ma_50_days'))
 
 
     startTime = time.time()
-    query = result.writeStream.outputMode('complete').option('numRows', 600).format("console").start().awaitTermination() 
+    query = result.writeStream.outputMode('complete').option('numRows', 600).format("console").trigger(once=True).start().awaitTermination() 
+    #query = result.writeStream.outputMode('complete').option('numRows', 600).format("console").start().awaitTermination() 
     endTime = time.time()
     print(endTime - startTime) 
  
